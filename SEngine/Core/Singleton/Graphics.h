db45ea908764139807edebd35d53ffd4503a7084
hebohang <hebohang1113@gmail.com>
2022-02-15 21:34:10 +0800

    小改

--- SEngine/Core/Singleton/Graphics.h
+++ SEngine/Core/Singleton/Graphics.h
@@ -61,18 +61,4 @@ private:
 	ComPtr<ID3D11RenderTargetView> pTarget;
 	ComPtr<ID3D11DepthStencilView> pDSV;
 	ComPtr<ID3D11Texture2D> pDepthStencilBuffer;				// 深度模板缓冲区
-
-	// Direct3D 11
-	ComPtr<ID3D11Device> m_pd3dDevice;							    // D3D11设备
-	ComPtr<ID3D11DeviceContext> m_pd3dImmediateContext;			    // D3D11设备上下文
-	ComPtr<IDXGISwapChain> m_pSwapChain;							// D3D11交换链
-	// Direct3D 11.1
-	ComPtr<ID3D11Device1> m_pd3dDevice1;							// D3D11.1设备
-	ComPtr<ID3D11DeviceContext1> m_pd3dImmediateContext1;			// D3D11.1设备上下文
-	ComPtr<IDXGISwapChain1> m_pSwapChain1;						    // D3D11.1交换链
-	// 常用资源
-	ComPtr<ID3D11Texture2D> m_pDepthStencilBuffer;					// 深度模板缓冲区
-	ComPtr<ID3D11RenderTargetView> m_pRenderTargetView;				// 渲染目标视图
-	ComPtr<ID3D11DepthStencilView> m_pDepthStencilView;				// 深度模板视图
-	D3D11_VIEWPORT m_ScreenViewport;								// 视口
 };
